8efcce8609b7571c2ad9e2fbf3227f102c828d1e
pedro-baierle <149621401+pedro-baierle@users.noreply.github.com>
2023-11-06 17:14:35 -0300

    Add files via upload
    Signed-off-by: pedro-baierle <149621401+pedro-baierle@users.noreply.github.com>

---
+++ reconhecimento_facial_2.py
@@ -0,0 +1,34 @@
+import face_recognition
+import cv2
+import time
+
+#########################################################
+
+eu  = face_recognition.load_image_file("/home/pedro/Documents/fotos_reconhecimento/eu_1.jpg")
+mae = face_recognition.load_image_file("/home/pedro/Documents/fotos_reconhecimento/mae_1.jpg")
+
+eu_encoding = face_recognition.face_encodings(eu)[0]
+mae_encoding = face_recognition.face_encodings(mae)[0]
+faces_conhecidas = [eu_encoding, mae_encoding]
+known_face_names = ['Pedro', 'Gladis']
+
+############################################################
+
+
+while True:
+	cap = cv2.VideoCapture(0)
+	ret, frame = cap.read()
+
+	rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	face_locations = face_recognition.face_locations(rgb_frame)
+	face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+	for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+		matches = face_recognition.compare_faces(faces_conhecidas, face_encoding)
+		
+########################################################################### 
+		if True in matches:
+			print('rosto conhecido')
+	time.sleep(1)
+	cap.release()
